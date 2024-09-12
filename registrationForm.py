3a5a365519f53a33ddb34db61154e1a3defd6bb4
Mohammed Rohman <159622932+cachemoneey@users.noreply.github.com>
2024-09-12 14:48:19 +0100

    Update registrationForm.py

--- registrationForm.py
+++ registrationForm.py
@@ -5,8 +5,26 @@ root.title("Registration Form")
 root.geometry("600x470")
 root.resizable(False,False)
 
+# Function to handle user registration
 def register():
-    print("Registered")
+    name_info = nameValue.get()
+    phone_info = phoneValue.get()
+    gender_info = genderValue.get()
+    email_info = emailValue.get()
+    
+    file = open(name_info + ".txt", "w")
+    file.write(name_info + "\n")
+    file.write(phone_info + "\n")
+    file.write(gender_info + "\n")
+    file.write(email_info + "\n")
+    file.close()
+    
+    nameEntry.delete(0, END)
+    phoneEntry.delete(0, END)
+    genderEntry.delete(0, END)
+    emailEntry.delete(0, END)
+    
+    Label(text="Registration success", fg="green", font=("Calibri")).place(x=250, y=430)
 
 Label(root, text="Python Registration Form", font="Arial, 25").pack(pady=50)
 
@@ -16,7 +34,6 @@ Label(text="Gender", font=23).place(x=100, y=250)
 Label(text="Email", font=23).place(x=100, y=300)
 
 # Entry
-
 nameValue = StringVar()
 phoneValue = StringVar()
 genderValue = StringVar()
